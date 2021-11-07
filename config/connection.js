0d025f88a0316b7e19b9e0b7899a604b67f9615b
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-07 17:40:24 -0600

    refactor code to create and connect to db

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
+// importing the environment variables
 require('dotenv').config();
 
+// importing the "Sequelize" constructor from the library 
 const Sequelize = require('sequelize');
 
+// creating the connection tp the database
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
