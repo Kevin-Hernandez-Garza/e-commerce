c42860245a25ace80228c9bd1b68dbc3674b2eb9
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-09 00:35:27 -0600

    code organized

--- models/index.js
+++ models/index.js
@@ -13,12 +13,10 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  // category_name
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  //category_name
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
