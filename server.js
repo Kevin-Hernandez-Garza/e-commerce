209ca12cca3e0b8fb7511722e91748a17dcdf9cd
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-11-09 16:28:15 -0600

    code refactoring

--- server.js
+++ server.js
@@ -20,6 +20,3 @@ sequelize.sync({ force: false }).then(() => {
     console.log(`App listening on port ${PORT}!`);
   });
 })
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
